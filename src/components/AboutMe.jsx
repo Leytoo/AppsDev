46466dd6958be514e6e20b0850b3d10fde27af7f
Leytoo <panimdim217@gmail.com>
2025-07-31 18:04:54 +0800

    Add files via upload

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      <p>
+        Hi! I'm <strong>Panimdim , Joselito</strong>, a 22-year-old male and currently a 3rd-year college student at Central Philippine College (CPC).
+        I'm passionate about learning and continuously striving to improve myself every day.
+        My goal is to finish my degree and build a successful future in the tech industry.
+        I believe in working hard, staying focused, and never giving up on my dreams.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="footer">© 2025 panimdim joselito</footer>;
+};
+
+export default Footer;
