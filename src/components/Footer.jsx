3e3da981beff80e18e8cb239a16571b42b64fc0f
Li2mee <panimdim217@gmail.com>
2025-07-31 20:35:21 +0800

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <footer className="footer">© 2025 panimdim joselito</footer>;
-};
-
-export default Footer;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const ProfilePicture = () => {
-  return (
-    <div className="profile-picture">
-        My Portfolio in Apps Development
-    </div>
-  );
-};
-
-export default ProfilePicture;
